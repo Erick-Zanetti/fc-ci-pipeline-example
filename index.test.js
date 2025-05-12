a3beb82b3796051a09f67afaea0e96d233587b3e
Erick <erick_98zanetti_98@hotmail.com>
2025-05-12 11:35:49 -0300

    comeback test

--- index.test.js
+++ index.test.js
@@ -4,9 +4,9 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-// test('multiplies 2 * 2 to equal 4', () => {
-//   expect(multiply(2, 2)).toBe(4);
-// });
+test('multiplies 2 * 2 to equal 4', () => {
+  expect(multiply(2, 2)).toBe(4);
+});
 
 // test('divides 4 / 2 to equal 2', () => {
 //     expect(divide(4, 2)).toBe(2);
